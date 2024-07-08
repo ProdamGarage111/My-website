40ee78c0e89f4672bdf6fcb8a50154682fa8a324
ProdamGarage111 <nata.simonenko.72@mail.ru>
2024-07-08 12:55:28 -0400

    Initial commit

---
+++ scrits.js
@@ -0,0 +1,10 @@
+function my_function(){
+    let my_image = document.getElementById("main_jaguar");
+    my_image.src = "images/jaguar_replace.jpeg";
+}
+console.log(window.innerWidth)
+console.log("Скрипт подгружен")
+console.log(navigator.userAgent)
+console.log(navigator.platform)
+console.log(location.href)
+console.log(history)
